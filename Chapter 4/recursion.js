5bca364c338db0bb9501c2e6bc8aa586e6024ab5
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-11-16 18:48:16 +0100

    Update recursion.js to fix RangeError when num is negative

--- Chapter 4/recursion.js
+++ Chapter 4/recursion.js
@@ -3,10 +3,12 @@ function isEven(num) {
         return true;
     else if (num == 1)
         return false;
+    else if (num < 0)
+        return isEven(-num);
     else
         return isEven(num - 2);
 }
 
 console.log(isEven(20));
 console.log(isEven(25));
-console.log(isEven(-23));
+console.log(isEven(-2));
