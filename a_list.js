60bab431e6c9983e36321bf9b96f4d5ce25e7372
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-11-24 21:17:49 +0100

    Solution for A LIST exercise

---
+++ a_list.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function arrayToList(array) {
+    let list = null;
+
+    for (let i = array.length - 1; i >= 0; i--) {
+        list = {
+            value: array[i],
+            rest: list,
+        };
+    }
+    return list;
+}
+
+function listToArray(list) {
+    let array = [];
+
+    for (let node = list; node != null; node = node.rest) {
+        array.push(node.value);
+    }
+
+    return array;
+}
+
+function prepend(value, list) {
+    return { value, rest: list };
+}
+
+function nth(list, n) {
+    if (!list) return undefined;
+    else if (n == 0) return list.value;
+    else return nth(list.rest, n - 1);
+}
